cf70c350b90388898d5df0726fc187bba6b6ba9b
Julio Romero <julio_ale21@hotmail.com>
2021-07-05 20:47:15 -0300

    chore: added container fluid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Calculator from "./components/Calculator";
-import { Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App d-flex align-items-center justify-content-center">
+    <Container fluid className="App d-flex justify-content-center">
       <Row className="w-100 d-flex flex-column align-items-center justify-content-center">
         <Calculator />
       </Row>
-    </div>
+    </Container>
   );
 }
 
