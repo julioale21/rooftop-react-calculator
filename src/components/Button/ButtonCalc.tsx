9415c28f122df198edc28c576cb4c4f260049c29
Julio Romero <julio_ale21@hotmail.com>
2021-07-05 20:47:51 -0300

    style: added styles

--- src/components/Button/ButtonCalc.tsx
+++ src/components/Button/ButtonCalc.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { Button, Col } from "react-bootstrap";
-
+import "./index.css";
 interface Props {
   value: string;
   span?: number;
-  background?: "primary" | "secondary" | "success" | "danger" | "warning";
+  shadow?: string;
+  background?: "primary" | "secondary" | "success" | "danger" | "warning" | "light";
   onClickEvent?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
-const ButtonCalc: React.FC<Props> = ({ value = "0", background = "secondary", span = 3 }) => {
+const ButtonCalc: React.FC<Props> = ({
+  value,
+  background = "light",
+  span = 3,
+  shadow = "gray",
+}) => {
   return (
     <Col className="p-1" xs={{ span: span }}>
-      <Button className="w-100 m-0 font-weight-bold" variant={background}>
-        {value}
+      <Button
+        className="button w-100 mx-0 my-1"
+        style={{ boxShadow: `2px 3px 4px ${shadow}` }}
+        variant={background}
+      >
+        <p className="font-weight-bold m-0" style={{ fontSize: "2rem" }}>
+          {value}
+        </p>
       </Button>
     </Col>
   );
