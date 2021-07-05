dd90dda7f1584d6fc3e0274155206bc5e744c3fb
Julio Romero <julio_ale21@hotmail.com>
2021-07-05 17:45:42 -0300

    chore: added calculator to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Calculator from "./components/Calculator";
+import { Row } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="App d-flex align-items-center justify-content-center">
+      <Row className="w-100 d-flex flex-column align-items-center justify-content-center">
+        <Calculator />
+      </Row>
     </div>
   );
 }
