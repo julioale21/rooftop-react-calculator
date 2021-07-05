1c150055fe1bcc521c8a7788b731b49f6d5ae771
Julio Romero <julio_ale21@hotmail.com>
2021-07-05 20:48:53 -0300

    style: added shadow

--- src/components/DisplayScreen/DisplayScreen.tsx
+++ src/components/DisplayScreen/DisplayScreen.tsx
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 
 const DisplayScreen: React.FC = () => {
   return (
-    <Container className="bg-dark px-3 py-2 mb-2">
+    <Container className="bg-dark px-3 py-2 mb-2 shadow-lg">
       <p className="display-4 text-white font-weight-bold text-right">0</p>
     </Container>
   );
--- src/components/Keypad/Keypad.tsx
+++ src/components/Keypad/Keypad.tsx
@@ -4,12 +4,12 @@ import ButtonCalc from "../Button";
 
 const Keypad: React.FC = () => {
   return (
-    <Container className="bg-light">
-      <Row>
+    <Container className="bg-dark mt-3 shadow-lg">
+      <Row className="p-3">
         <ButtonCalc value="7" />
         <ButtonCalc value="8" />
         <ButtonCalc value="9" />
-        <ButtonCalc value="DEL" />
+        <ButtonCalc background="primary" shadow="#3C8DAD" value="DEL" />
 
         <ButtonCalc value="5" />
         <ButtonCalc value="4" />
@@ -26,8 +26,8 @@ const Keypad: React.FC = () => {
         <ButtonCalc value="/" />
         <ButtonCalc value="x" />
 
-        <ButtonCalc span={6} value="RESET" />
-        <ButtonCalc span={6} value="=" />
+        <ButtonCalc background="primary" shadow="#3C8DAD" span={6} value="RESET" />
+        <ButtonCalc background="danger" shadow="#FF616D" span={6} value="=" />
       </Row>
     </Container>
   );
