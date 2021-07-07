d5b810892bd4390e54b5dc2071a287708f028e37
Julio Romero <julio_ale21@hotmail.com>
2021-07-07 20:55:17 -0300

    chore: connected click event with actions

--- src/components/Button/ButtonCalc.tsx
+++ src/components/Button/ButtonCalc.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { Button, Col } from "react-bootstrap";
+import {
+  addition,
+  subtraction,
+  equal,
+  division,
+  multiplication,
+  del,
+  resetDisplay,
+  updateDisplay,
+} from "../../redux/actions/actions";
+import { useDispatch } from "react-redux";
 import "./index.css";
 interface Props {
   value: string;
@@ -14,12 +25,43 @@ const ButtonCalc: React.FC<Props> = ({
   span = 3,
   shadow = "gray",
 }) => {
+  const dispatch = useDispatch();
+
+  const handleClick = () => {
+    switch (value) {
+      case "RESET":
+        dispatch(resetDisplay());
+        break;
+      case "DEL":
+        dispatch(del());
+        break;
+      case "+":
+        dispatch(addition());
+        break;
+      case "-":
+        dispatch(subtraction());
+        break;
+      case "%":
+        dispatch(division());
+        break;
+      case "x":
+        dispatch(multiplication());
+        break;
+      case "=":
+        dispatch(equal());
+        break;
+      default:
+        dispatch(updateDisplay(value));
+    }
+  };
+
   return (
     <Col className="p-1" xs={{ span: span }}>
       <Button
         className="button w-100 mx-0 my-1"
         style={{ boxShadow: `2px 3px 4px ${shadow}` }}
         variant={background}
+        onClick={handleClick}
       >
         <p className="font-weight-bold m-0" style={{ fontSize: "2rem" }}>
           {value}
