90ea771a9cb0576e6fb3defcbcd2f0845e734594
Julio Romero <julio_ale21@hotmail.com>
2021-07-07 20:55:34 -0300

    chore: added store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Calculator from "./components/Calculator";
 import { Container, Row } from "react-bootstrap";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import "./App.css";
 
 function App() {
   return (
-    <Container fluid className="App d-flex justify-content-center">
-      <Row className="w-100 d-flex flex-column align-items-center justify-content-center">
-        <Calculator />
-      </Row>
-    </Container>
+    <Provider store={store}>
+      <Container fluid className="App d-flex justify-content-center">
+        <Row className="w-100 d-flex flex-column align-items-center justify-content-center">
+          <Calculator />
+        </Row>
+      </Container>
+    </Provider>
   );
 }
 
