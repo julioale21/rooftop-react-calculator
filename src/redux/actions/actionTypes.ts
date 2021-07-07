aa9a3435879d1960fe4d848bcffa9194273e1d7b
Julio Romero <julio_ale21@hotmail.com>
2021-07-07 20:53:24 -0300

    chore: added action types

---
+++ src/redux/actions/actionTypes.ts
@@ -0,0 +1,8 @@
+export const RESET = "RESET";
+export const UPDATE = "UPDATE";
+export const DEL = "DEL";
+export const ADD = "ADD";
+export const SUBTRACT = "SUBTRACT";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE";
+export const EQUAL = "CALCULATE";
