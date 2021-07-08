6a260ef6e40d44651548703348f0757fa0aed038
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-07-08 01:32:47 -0300

    Update DisplayScreen.tsx

--- src/components/DisplayScreen/DisplayScreen.tsx
+++ src/components/DisplayScreen/DisplayScreen.tsx
@@ -8,7 +8,7 @@ interface Props {
 const DisplayScreen: React.FC<Props> = ({ value }) => {
   return (
     <Container
-      className="bg-dark px-3 py-2 mb-2 shadow-lg overflow-auto"
+      className="bg-dark px-3 py-2 mb-2 shadow-lg overflow-hidden"
       style={{ minHeight: "100px" }}
     >
       <p className="display-4 text-white font-weight-bold text-right">{value}</p>
