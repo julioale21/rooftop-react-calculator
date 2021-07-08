2abd1197971a13dc6b2d14ec5dbe14691f66ae17
Julio Romero <julio_ale21@hotmail.com>
2021-07-07 23:05:16 -0300

    chore: added header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,19 @@ import { Container, Row } from "react-bootstrap";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Provider store={store}>
       <Container fluid className="App d-flex justify-content-center">
-        <Row className="w-100 d-flex flex-column align-items-center justify-content-center">
-          <Calculator />
+        <Row>
+          <Header />
+          <Row className="w-100 d-flex flex-column align-items-center justify-content-center">
+            <Calculator />
+          </Row>
+          <Footer />
         </Row>
       </Container>
     </Provider>
